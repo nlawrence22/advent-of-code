5b80385c5f166f7063226884f8805ffc8aaaf0be
Nicholas Lawrence <nlawrence@wsu.edu>
2020-12-11 23:29:02 -0800

    day12 part 2

--- day12/day12.py
+++ day12/day12.py
@@ -50,73 +50,117 @@ def move_in_dir(direction, units, current_x, current_y):
 
     return new_x, new_y
 
-def get_dir_from_heading(current_dir):
-    direction = None
+# def get_dir_from_heading(current_dir):
+#     direction = None
 
-    if current_dir == NORTH_DIR:
-        direction = NORTH
-    elif current_dir == EAST_DIR:
-        direction = EAST
-    elif current_dir == SOUTH_DIR:
-        direction = SOUTH
-    elif current_dir == WEST_DIR:
-        direction = WEST
-    else:
-        raise Exception('Unknown heading')
+#     if current_dir == NORTH_DIR:
+#         direction = NORTH
+#     elif current_dir == EAST_DIR:
+#         direction = EAST
+#     elif current_dir == SOUTH_DIR:
+#         direction = SOUTH
+#     elif current_dir == WEST_DIR:
+#         direction = WEST
+#     else:
+#         raise Exception('Unknown heading')
 
-    return direction
+#     return direction
 
-def move_forward(current_dir, units, current_x, current_y):
-    heading = get_dir_from_heading(current_dir)
-    return move_in_dir(heading, units, current_x, current_y)
-
-def get_new_heading(current_dir, direction, units):
-    print('getting new heading!')
-    print('current heading = {}, instruction= {}, {}'.format(current_dir, direction, units))
-    value = current_dir
-
-    if direction is LEFT:
-        value = current_dir - units
-    elif direction is RIGHT:
-        value = current_dir + units
-
-    if value < 0:
-        return value + 360
-    elif value > 360:
-        return value - 360
-    elif value == 360:
-        return 0
-    else:
-        return value
-
-def move_ship(direction, units, current_x, current_y, current_dir):
+def move_forward(units, waypoint_x, waypoint_y, current_x, current_y):
     new_x = current_x
     new_y = current_y
-    new_dir = current_dir
+    x_change = waypoint_x
+    y_change = waypoint_y
+
+    if waypoint_x < 0:
+        x_change = (-1 * abs(waypoint_x * units))
+    else:
+        x_change = (waypoint_x * units)
+    
+    if waypoint_y < 0:
+        y_change = (-1 * abs(waypoint_y * units))
+    else:
+        y_change = (waypoint_y * units)
+    
+    return current_x + x_change, current_y + y_change
+
+""" 
+WXL
+XSX
+RXX
+
+-1,1
+L 1, 1
+R -1, -1    
+
+XLX
+WSX
+XRX
+
+-1, 0
+L 0, 1
+R 0, -1
+
+LXX
+XSX
+WXR
+
+-1, -1
+L -1, 1
+R 1, -1 
+"""
+
+def rotate_waypoint(direction, units, waypoint_x, waypoint_y):
+    new_x = waypoint_x
+    new_y = waypoint_y
+
+    if units == 180:
+        new_x = -waypoint_x
+        new_y = -waypoint_y
+    elif units == 90:
+        if direction is LEFT:
+            new_x = -waypoint_y
+            new_y = waypoint_x
+        elif direction is RIGHT:
+            new_x = waypoint_y
+            new_y = -waypoint_x
+    elif units == 270:
+        if direction is LEFT:
+            new_x = waypoint_y
+            new_y = -waypoint_x
+        elif direction is RIGHT:
+            new_x = -waypoint_y
+            new_y = waypoint_x
+
+    return new_x, new_y
+
+def move_waypoint(direction, units, current_x, current_y):
+    new_x = current_x
+    new_y = current_y
 
     if direction is NORTH or direction is EAST or direction is SOUTH or direction is WEST:
         new_x, new_y = move_in_dir(direction, units, current_x, current_y)
-    elif direction is FORWARD:
-        new_x, new_y =  move_forward(current_dir, units, current_x, current_y)
     elif direction is LEFT or direction is RIGHT:
-        new_dir = get_new_heading(current_dir, direction, units)
+        new_x, new_y = rotate_waypoint(direction, units, waypoint_x, waypoint_y)
     else:
         raise Exception('Unknown instruction')
 
-    return new_x, new_y, new_dir
+    return new_x, new_y
 
 def calc_manhattan_distance(current_x, current_y):
     return abs(current_x) + abs(current_y)
 
-
-facing_dir = EAST_DIR
 x_pos = 0
 y_pos = 0
-# print('x={}, y={}, heading={}'.format(x_pos, y_pos, facing_dir))
+waypoint_x = 10
+waypoint_y = 1
+
 for direction, units in get_direction_input('input.txt'):
-    # print('instruction=', direction, units)
-    x_pos, y_pos, facing_dir = move_ship(direction, units, x_pos, y_pos, facing_dir)
-    # print('x={}, y={}, heading={}'.format(x_pos, y_pos, facing_dir))
+    print('instruction=', direction, units)
+    if direction is FORWARD:
+        x_pos, y_pos = move_forward(units, waypoint_x, waypoint_y, x_pos, y_pos)
+    else:
+        waypoint_x, waypoint_y = move_waypoint(direction, units, waypoint_x, waypoint_y)
 
 print(calc_manhattan_distance(x_pos, y_pos)) 
 
